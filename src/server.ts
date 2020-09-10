e5374615b0b36bf85e464ce0bd499bddbb0c3ef4
Chris Burke <chrisburke.jr@gmail.com>
2020-09-10 09:31:00 -0400

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import { testRouter } from './routes/test/testRouter';
-import express from 'express';
+import express, {Request, Response} from 'express';
 import bodyParser from 'body-parser';
 
 const cors = require('cors');
@@ -10,6 +10,6 @@ app.use(bodyParser.urlencoded({ limit: '15mb', extended: false }));
 app.use(bodyParser.json({ limit: '15mb' }));
 
 app.use(testRouter);
-app.get('*', (req: any, res: any) => res.send('Not Found'));
+app.get('*', (req: Request, res: Response) => res.send('Not Found'));
 
 module.exports = app;
